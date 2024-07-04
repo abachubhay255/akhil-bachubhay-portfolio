0472c5707fabff51296c18fa1742924fb8093d7d
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2024-07-04 13:46:43 -0400

    basic mantine vite app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+import { Home } from "./Home";
+
+function App() {
+  return (
+    <MantineProvider defaultColorScheme="dark">
+      <Home />
+    </MantineProvider>
+  );
+}
+
+export default App;
