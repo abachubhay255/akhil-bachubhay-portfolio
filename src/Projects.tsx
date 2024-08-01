e7b7f323c4b397602b5e67eefe04347118122d4b
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2024-08-01 18:46:27 -0400

    updated projects and skills

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,9 +1,25 @@
-import { Badge, Card, Group, Button, Text, SimpleGrid } from "@mantine/core";
-import { IconBarbell, IconBrandGithub, IconMovie } from "@tabler/icons-react";
+import {
+  Badge,
+  Card,
+  Group,
+  Button,
+  Text,
+  SimpleGrid,
+  List,
+  Stack,
+  Divider,
+} from "@mantine/core";
+import {
+  IconBallAmericanFootball,
+  IconBarbell,
+  IconBrandGithub,
+  IconMovie,
+  IconWorldWww,
+} from "@tabler/icons-react";
 
 export function Projects() {
   return (
-    <SimpleGrid cols={{ base: 1, sm: 2 }}>
+    <SimpleGrid cols={{ base: 1, sm: 3 }}>
       {data.map((d) => (
         <BadgeCard key={d.title} {...d} />
       ))}
@@ -13,13 +29,14 @@ export function Projects() {
 
 type Project = {
   title: string;
-  description: string;
+  bullets: string[];
   skills: string[];
   src: string;
   icon: React.ReactNode;
+  demo?: string;
 };
 
-function BadgeCard({ title, description, skills, src, icon }: Project) {
+function BadgeCard({ title, bullets, skills, src, icon, demo }: Project) {
   const technologies = skills.map((skill, i) => (
     <Badge key={skill} variant="light" color={i % 2 === 0 ? "gray" : "blue"}>
       {skill}
@@ -28,63 +45,107 @@ function BadgeCard({ title, description, skills, src, icon }: Project) {
 
   return (
     <Card withBorder radius="md" p="md" bg="dark.7">
-      <Card.Section
-        px="md"
-        pb="md"
-        mt={1}
-        style={{ borderBottom: "1px solid var(--mantine-color-dark-4)" }}
-        h="100%"
-      >
-        <Group justify="apart">
-          {icon}
-          <Text fz="lg" fw={500}>
-            {title}
-          </Text>
-        </Group>
-        <Text fz="sm" mt="xs">
-          {description}
-        </Text>
-      </Card.Section>
+      <Stack gap="xs">
+        <Card.Section
+          px="md"
+          pb="sm"
+          mt={1}
+          style={{ borderBottom: "1px solid var(--mantine-color-dark-4)" }}
+        >
+          <Group justify="apart">
+            {icon}
+            <Text fz="lg" fw={500}>
+              {title}
+            </Text>
+          </Group>
+        </Card.Section>
+        <List size="sm" pr="sm" h={{base: 175, sm: 175, lg: 150}}>
+          <Stack gap="xs">
+            {bullets.map((bullet, i) => (
+              <List.Item key={i}>{bullet}</List.Item>
+            ))}
+          </Stack>
+        </List>
 
-      <Card.Section
-        px="md"
-        pb="md"
-        style={{ borderBottom: "1px solid var(--mantine-color-dark-4)" }}
-      >
-        <Group gap={7} mt="xs">
+        <Divider label="Technologies" labelPosition="center" />
+
+        <Group gap={7} align="start" h={75}>
           {technologies}
         </Group>
-      </Card.Section>
 
-      <Group mt="xs">
-        <Button
-          fullWidth
-          color="#141414"
-          component="a"
-          href={src}
-          target="_blank"
-          leftSection={<IconBrandGithub size={18} />}
-        >
-          View on GitHub
-        </Button>
-      </Group>
+        <Divider />
+
+        <Group>
+          {demo && (
+            <Button
+              variant="gradient"
+              flex={1}
+              component="a"
+              href={demo}
+              target="_blank"
+              leftSection={<IconWorldWww size={18} />}
+            >
+              View Demo
+            </Button>
+          )}
+          <Button
+            fullWidth={demo ? false : true}
+            color="#141414"
+            component="a"
+            href={src}
+            target="_blank"
+            leftSection={<IconBrandGithub size={18} />}
+          >
+            View on GitHub
+          </Button>
+        </Group>
+      </Stack>
     </Card>
   );
 }
 
 const data = [
   {
+    title: "Redzone Insights",
+    bullets: [
+      "Developed an NFL stats and betting app.",
+      "Integrated real-time scores, detailed standings, and a unique parlays section.",
+      "Implemented player projections powered by OpenAI API.",
+      "Deployed on Azure Web Apps and uses Azure Blob Storage for caching.",
+    ],
+    skills: [
+      "TypeScript",
+      "Vite",
+      "React",
+      "Node.js",
+      "Express",
+      "GraphQL",
+      "OpenAI API",
+      "Azure",
+      "Mantine UI",
+    ],
+    src: "https://github.com/abachubhay255/redzone-insights",
+    icon: <IconBallAmericanFootball />,
+    demo: "https://redzone-insights.akhilbachubhay.com/",
+  },
+  {
     title: "GymBro",
-    description:
-      "Developed a dynamic front-end mobile application designed as a fitness-centric social media platform. This project features user-generated posts, comprehensive profile management, and real-time messaging to foster community engagement and support among fitness enthusiasts.",
+    bullets: [
+      "Developed a fitness-centric social media mobile app.",
+      "Implemented features such as user-generated posts, profile management, and real-time messaging.",
+      "Designed an intuitive and engaging user interface with UI Kitten.",
+    ],
     skills: ["React Native", "TypeScript", "Javascript", "UI Kitten"],
     src: "https://github.com/abachubhay255/GymBro",
     icon: <IconBarbell />,
   },
   {
     title: "Movie Paradise",
-    description:
-      "Developed an organizational movie web app featuring secure user management, CRUD functionalities for movies and lists, and advanced search capabilities using TheMovieDB API.",
+    bullets: [
+      "Created a web app with secure user management and CRUD functionalities for movies and lists.",
+      "Implemented advanced search capabilities using TheMovieDB API.",
+      "Utilized Angular and Material UI for frontend design and Node.js with Express for backend.",
+    ],
     skills: [
       "Angular",
       "TypeScript",
@@ -92,6 +153,7 @@ const data = [
       "Express.js",
       "MongoDB",
       "JWT",
+      "Material UI",
     ],
     src: "https://github.com/abachubhay255/MovieParadiseProject",
     icon: <IconMovie />,
--- src/Skills.tsx
+++ src/Skills.tsx
@@ -58,6 +58,7 @@ const backEndSkills = [
   "MongoDB",
   "Grails",
   "Kafka",
+  "SQL"
 ];
 
 const languages = [
